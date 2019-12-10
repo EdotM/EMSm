007b7c21c36659e341b4f032ccbf9d36cf2c6fbe
Manfred Ehrengruber <em@eforge.net>
2019-12-10 20:43:23 +0100

    Blinky-Sample added.

---
+++ EMSm.Samples/Blinky/GPIO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Blinky
+{
+    /// <summary>
+    /// GPIO: Representation of the general purpose I/Os
+    /// </summary>
+    internal class GPIO
+    {
+        private bool lamp = false;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the lamp is switched on or off.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if lamp in switched on; otherwise, <c>false</c>.
+        /// </value>
+        public bool Lamp
+        {
+            get => this.lamp;
+            set
+            {
+                this.lamp = value;
+                if (this.lamp)
+                    Console.WriteLine("Lamp switched on");
+                else
+                    Console.WriteLine("Lamp switched off");
+            }
+        }
+
+        /// <summary>
+        /// Toggles the lamp.
+        /// </summary>
+        public void ToggleLamp()
+        {
+            this.Lamp = !this.Lamp;
+        }
+    }
+}
